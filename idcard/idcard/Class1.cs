6ca040b64cb94d053e7112647f5d299c0da15e48
ileegone <ileegone@qq.com>
2019-03-17 00:24:07 +0800

    1-add

---
+++ idcard/idcard/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace idcard
+{
+    public class readid
+    {
+        public int getinfo() {
+            return 1;
+        }
+    }
+}
